506ae5a46ebe8eb7cfcd573317c9ece968f7fa41
namartiinss <nathaliamartins0708@gmail.com>
2022-05-11 23:07:37 -0300

    aula 2 nlw

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-// function onScroll() {
-//     if () {
-//     navigation.classList.add('scroll')
-//     }
-// }
+function onScroll() {
+
+    
+    if (scrollY > 0 ) {
+    navigation.classList.add('scroll')
+    } else{
+        navigation.classList.remove('scroll')
+    }
+}
+
+function openMenu(){
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu-expanded')
+}
